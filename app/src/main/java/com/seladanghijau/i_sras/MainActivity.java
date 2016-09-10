b7e128dc2f0604f026ceec4a0d2e4cec6342c913
Nadzmi-Idzham <nadzmiidzham@gmail.com>
2016-09-10 18:00:01 +0800

    Initial commit

---
+++ app/src/main/java/com/seladanghijau/i_sras/MainActivity.java
@@ -0,0 +1,11 @@
+package com.seladanghijau.i_sras;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
