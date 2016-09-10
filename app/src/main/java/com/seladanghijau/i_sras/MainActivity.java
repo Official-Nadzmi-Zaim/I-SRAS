28103676c13a3abe34d9e72fbd0449b1dc5cf53d
Seladang Hijau <nadzmiidzham@gmail.com>
2016-09-10 20:15:53 +0800

    Update MainActivity.java

--- app/src/main/java/com/seladanghijau/i_sras/MainActivity.java
+++ app/src/main/java/com/seladanghijau/i_sras/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) { // testing updates
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
