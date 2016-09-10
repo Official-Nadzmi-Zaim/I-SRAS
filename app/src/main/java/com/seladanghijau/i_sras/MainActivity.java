db2e55f59fd236c361d30339217b39798e779911
Nadzmi-Idzham <nadzmiidzham@gmail.com>
2016-09-10 20:28:01 +0800

    updates

--- app/src/main/java/com/seladanghijau/i_sras/MainActivity.java
+++ app/src/main/java/com/seladanghijau/i_sras/MainActivity.java
@@ -4,10 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    protected void onCreate(Bundle savedInstanceState) { // testing updates
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
-        // try saja 4
     }
 }
