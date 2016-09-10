2db056ff3101abd864f362123cd2d6b33110b195
officialzaim <officialzaim@gmail.com>
2016-09-11 07:15:24 +0800

    Done Fragment udpates

---
+++ app/src/main/java/com/seladanghijau/i_sras/Fragment/EnvironmentFragment.java
@@ -0,0 +1,32 @@
+package com.seladanghijau.i_sras.Fragment;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.seladanghijau.i_sras.R;
+
+public class EnvironmentFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Dalam ini untuk apa apa code selain set listener dgn findviewbyid
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragmentenvironment, container, false);
+        //inflate fragment layout kat sini
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //kat sini set findviewby id dgn listener e.g Button btnNext = (Button) getActivity().findViewById(R.id.btnNext2);
+    }
+}
--- app/src/main/java/com/seladanghijau/i_sras/MainActivity.java
+++ app/src/main/java/com/seladanghijau/i_sras/MainActivity.java
@@ -1,11 +1,34 @@
 package com.seladanghijau.i_sras;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    Fragment homeFragment, communityFragment, workplaceFragment, environmentFragment, marketplaceFragment, reportFragment;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Dekat sini akan guna getFragmentManager untuk find id fragment
+        homeFragment = getFragmentManager().findFragmentById(R.id.HomeFragment);
+        workplaceFragment = getFragmentManager().findFragmentById(R.id.WorkplaceFragment);
+        environmentFragment = getFragmentManager().findFragmentById(R.id.EnvironmentFragment);
+        marketplaceFragment = getFragmentManager().findFragmentById(R.id.MarketplaceFragment);
+        communityFragment = getFragmentManager().findFragmentById(R.id.CommunityFragment);
+        reportFragment = getFragmentManager().findFragmentById(R.id.ReportFragment);
+
+        //Guna FragmentTransaction untuk buat fragment visible. HomeFragment je dulu kat sini
+        FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        fragmentTransaction.show(homeFragment);
+        fragmentTransaction.hide(communityFragment);
+        fragmentTransaction.hide(workplaceFragment);
+        fragmentTransaction.hide(environmentFragment);
+        fragmentTransaction.hide(marketplaceFragment);
+        fragmentTransaction.hide(reportFragment);
+        fragmentTransaction.commit();
     }
 }
