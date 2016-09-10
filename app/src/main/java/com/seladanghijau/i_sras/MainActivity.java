38fb25940fb0b14e4a2b8b5ef03a90b7ef7c6c79
Nadzmi-Idzham <nadzmiidzham@gmail.com>
2016-09-10 20:21:08 +0800

    updates

--- app/src/main/java/com/seladanghijau/i_sras/MainActivity.java
+++ app/src/main/java/com/seladanghijau/i_sras/MainActivity.java
@@ -8,6 +8,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // nak try
+        // try saja 2
     }
 }
