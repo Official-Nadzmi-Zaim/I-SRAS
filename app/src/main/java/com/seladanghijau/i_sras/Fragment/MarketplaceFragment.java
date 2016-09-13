b48b089e169a3bd66d9a5e046d6afd8fa7e482f4
officialzaim <officialzaim@gmail.com>
2016-09-13 17:19:41 +0800

    Done

--- app/src/main/java/com/seladanghijau/i_sras/Fragment/MarketplaceFragment.java
+++ app/src/main/java/com/seladanghijau/i_sras/Fragment/MarketplaceFragment.java
@@ -9,19 +9,89 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
 
 import com.seladanghijau.i_sras.R;
+import com.seladanghijau.i_sras.adapter.SoalanAdapter;
+import com.seladanghijau.i_sras.dtos.Answer;
+import com.seladanghijau.i_sras.dtos.Soalan;
+import com.seladanghijau.i_sras.helper.Helper;
+import com.seladanghijau.i_sras.providers.SoalanProvider;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MarketplaceFragment extends Fragment {
 
     Fragment environmentFragment, marketplaceFragment, reportFragment;
     Button btnPreviousM, btnNextM;
+    List<Soalan> soalanList, soalanList2, soalanList3, soalanList4, soalanList5, soalanList6, soalanList7, soalanList8;
+    ListView lvKeyArea1_1, lvKeyArea2_1,lvKeyArea2_2, lvKeyArea3_1, lvKeyArea3_2, lvKeyArea3_3, lvKeyArea3_4, lvKeyArea4_1;
+    SoalanAdapter soalanAdapter, soalanAdapter2, soalanAdapter3, soalanAdapter4, soalanAdapter5, soalanAdapter6, soalanAdapter7, soalanAdapter8;
+    List<Answer> answerList, answerList2, answerList3, answerList4, answerList5, answerList6, answerList7, answerList8;
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //Dalam ini untuk apa apa code selain set listener dgn findviewbyid
+        answerList = new ArrayList<>();
+        answerList2 = new ArrayList<>();
+        answerList3 = new ArrayList<>();
+        answerList4 = new ArrayList<>();
+        answerList5 = new ArrayList<>();
+        answerList6 = new ArrayList<>();
+        answerList7 = new ArrayList<>();
+        answerList8 = new ArrayList<>();
+
+        soalanList = new ArrayList<>();
+        soalanList2 = new ArrayList<>();
+        soalanList3 = new ArrayList<>();
+        soalanList4 = new ArrayList<>();
+        soalanList5 = new ArrayList<>();
+        soalanList6 = new ArrayList<>();
+        soalanList7 = new ArrayList<>();
+        soalanList8 = new ArrayList<>();
+
+        //soalanList = SoalanProvider.loadSoalanBasedOnCategory(1);
+        soalanList = SoalanProvider.loadSoalanBasedOnRangeAndCategory(4, 1, 1);
+        soalanList2 = SoalanProvider.loadSoalanBasedOnRangeAndCategory(4, 2, 6);
+        soalanList3 = SoalanProvider.loadSoalanBasedOnRangeAndCategory(4, 7, 8);
+        soalanList4 = SoalanProvider.loadSoalanBasedOnRangeAndCategory(4, 9, 12);
+        soalanList5 = SoalanProvider.loadSoalanBasedOnRangeAndCategory(4, 13, 13);
+        soalanList6 = SoalanProvider.loadSoalanBasedOnRangeAndCategory(4, 14, 14);
+        soalanList7 = SoalanProvider.loadSoalanBasedOnRangeAndCategory(4, 15, 15);
+        soalanList8 = SoalanProvider.loadSoalanBasedOnRangeAndCategory(4, 16, 18);
+
+
+        //answerlist
+        answerList = CreateAnswer(soalanList);
+        answerList2 = CreateAnswer(soalanList2);
+        answerList3 = CreateAnswer(soalanList3);
+        answerList4 = CreateAnswer(soalanList4);
+        answerList5 = CreateAnswer(soalanList5);
+        answerList6 = CreateAnswer(soalanList6);
+        answerList7 = CreateAnswer(soalanList7);
+        answerList8 = CreateAnswer(soalanList8);
+
+
+        soalanAdapter = new SoalanAdapter(getActivity(), R.layout.soalanlistitem, soalanList, answerList);
+        soalanAdapter2 = new SoalanAdapter(getActivity(), R.layout.soalanlistitem, soalanList2, answerList2);
+        soalanAdapter3 = new SoalanAdapter(getActivity(), R.layout.soalanlistitem, soalanList3, answerList3);
+        soalanAdapter4 = new SoalanAdapter(getActivity(), R.layout.soalanlistitem, soalanList4, answerList4);
+        soalanAdapter5 = new SoalanAdapter(getActivity(), R.layout.soalanlistitem, soalanList5, answerList5);
+        soalanAdapter6 = new SoalanAdapter(getActivity(), R.layout.soalanlistitem, soalanList6, answerList6);
+        soalanAdapter7 = new SoalanAdapter(getActivity(), R.layout.soalanlistitem, soalanList7, answerList7);
+        soalanAdapter8 = new SoalanAdapter(getActivity(), R.layout.soalanlistitem, soalanList8, answerList8);
+
+        soalanAdapter.notifyDataSetChanged();
+        soalanAdapter2.notifyDataSetChanged();
+        soalanAdapter3.notifyDataSetChanged();
+        soalanAdapter4.notifyDataSetChanged();
+        soalanAdapter5.notifyDataSetChanged();
+        soalanAdapter6.notifyDataSetChanged();
+        soalanAdapter7.notifyDataSetChanged();
+        soalanAdapter8.notifyDataSetChanged();
     }
 
     @Nullable
@@ -38,6 +108,34 @@ public class MarketplaceFragment extends Fragment {
         environmentFragment = getFragmentManager().findFragmentById(R.id.EnvironmentFragment);
         marketplaceFragment = getFragmentManager().findFragmentById(R.id.MarketplaceFragment);
         reportFragment = getFragmentManager().findFragmentById(R.id.ReportFragment);
+
+        lvKeyArea1_1 = (ListView) getActivity().findViewById(R.id.lvKeyArea1_1_1c);
+        lvKeyArea2_1 = (ListView) getActivity().findViewById(R.id.lvKeyArea2_1_2c);
+        lvKeyArea2_2 = (ListView) getActivity().findViewById(R.id.lvKeyArea2_2_3c);
+        lvKeyArea3_1 = (ListView) getActivity().findViewById(R.id.lvKeyArea3_1_4c);
+        lvKeyArea3_2 = (ListView) getActivity().findViewById(R.id.lvKeyArea3_2_5c);
+        lvKeyArea3_3 = (ListView) getActivity().findViewById(R.id.lvKeyArea3_3_6c);
+        lvKeyArea3_4 = (ListView) getActivity().findViewById(R.id.lvKeyArea3_4_7c);
+        lvKeyArea4_1 = (ListView) getActivity().findViewById(R.id.lvKeyArea4_1_8c);
+
+        lvKeyArea1_1.setAdapter(soalanAdapter);
+        lvKeyArea2_1.setAdapter(soalanAdapter2);
+        lvKeyArea2_2.setAdapter(soalanAdapter3);
+        lvKeyArea3_1.setAdapter(soalanAdapter4);
+        lvKeyArea3_2.setAdapter(soalanAdapter5);
+        lvKeyArea3_3.setAdapter(soalanAdapter6);
+        lvKeyArea3_4.setAdapter(soalanAdapter7);
+        lvKeyArea4_1.setAdapter(soalanAdapter8);
+
+
+        Helper.getListViewSize(lvKeyArea1_1, 4);
+        Helper.getListViewSize(lvKeyArea2_1, 2);
+        Helper.getListViewSize(lvKeyArea2_2, 4);
+        Helper.getListViewSize(lvKeyArea3_1, 3);
+        Helper.getListViewSize(lvKeyArea3_2, 1);
+        Helper.getListViewSize(lvKeyArea3_3, 4);
+        Helper.getListViewSize(lvKeyArea3_4, 4);
+        Helper.getListViewSize(lvKeyArea4_1, 4);
 
         btnPreviousM = (Button) getActivity().findViewById(R.id.btnPreviousM);
         btnNextM = (Button) getActivity().findViewById(R.id.btnNextM);
@@ -66,5 +164,20 @@ public class MarketplaceFragment extends Fragment {
                     break;
             }
         }
+    }
+
+    //Create answer list based on soalan id
+    public List<Answer> CreateAnswer(List<Soalan> soalanList){
+
+        List<Answer> answerList = new ArrayList<>();
+
+        for (int i=0; i<soalanList.size(); i++){
+            Answer answer = new Answer();
+            answer.setAnswerId(soalanList.get(i).getSoalanId());
+
+            answerList.add(answer);
+        }
+
+        return answerList;
     }
 }
